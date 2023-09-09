c0536431e54e71c29cce496b83178e609a06d5e1
Kirionenko <misha.kirionenko03@gmail.com>
2023-09-10 02:18:26 +0500

    Version 1.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'https://alcocritics.vercel.app/:path*',
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
