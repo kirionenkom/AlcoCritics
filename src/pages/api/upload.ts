94a7793f54d68ef6e91d5124b78c7abf1866b35a
Kirionenko <misha.kirionenko03@gmail.com>
2023-09-10 03:11:26 +0500

    Version 1.0

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -2,28 +2,30 @@ import multer from 'multer';
 import { Request, Response } from 'express';
 
 const fs = require('fs');
-const upload = multer({dest: 'public/content-images'})
+const upload = multer({ dest: 'media' });
 
 export const config = {
 	api: {
-		bodyParser: false
-	}
-}
+		bodyParser: false,
+	},
+};
 
 export default async function NewDrink(req: Request, res: Response) {
 	try {
 		upload.single('image')(req, res, (err) => {
 			if (err || !req.file) {
-				console.error(err)
-				return res.status(500).json('Файл не найден')
+				console.error(err);
+				return res.status(500).json('Файл не найден');
 			}
-			const {path} = req.file;
-			const pathWithExt = `${path}.jpg`
-
-			fs.renameSync(path, pathWithExt)
-			return res.status(200).json(pathWithExt.slice(6, pathWithExt.length).replaceAll('\\', '/'))
-		})
+			const { path } = req.file;
+			const pathWithExt = `${path}.jpg`;
+			console.log(pathWithExt);
+			fs.renameSync(path, pathWithExt);
+			return res
+				.status(200)
+				.json(pathWithExt.slice(6, pathWithExt.length).replaceAll('\\', '/'));
+		});
 	} catch (error) {
-		return res.status(500).json('Произошла ошибка при запросе')
+		return res.status(500).json('Произошла ошибка при запросе');
 	}
 }
