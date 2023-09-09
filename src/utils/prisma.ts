f6d4527de02f6f65bb5ff3585e03f52f9af0ccae
Kirionenko <misha.kirionenko03@gmail.com>
2023-09-09 16:21:07 +0500

    Version 1.0

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@prisma/client'
-declare let global: { prisma: PrismaClient }
+import { PrismaClient } from './generated/client';
+declare let global: { prisma: PrismaClient };
 
-let prisma: PrismaClient
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient()
+	prisma = new PrismaClient();
 } else {
 	if (!global.prisma) {
-		global.prisma = new PrismaClient()
+		global.prisma = new PrismaClient();
 	}
-	prisma = global.prisma
+	prisma = global.prisma;
 }
 
-export default prisma
+export default prisma;
