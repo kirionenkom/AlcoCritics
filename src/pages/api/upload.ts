01db024f04ef8b525ff8e40a7c97efc6fd0a4ecf
Mikhail Kirionenko <102353134+WinoHare@users.noreply.github.com>
2023-09-10 13:43:15 +0500

    Update upload.ts

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,30 +1,9 @@
-import multer from 'multer';
-import { Request, Response } from 'express';
+import { put } from '@vercel/blob';
 
-const fs = require('fs');
-const upload = multer({ dest: 'images' });
+export const runtime = 'edge';
 
-export const config = {
-	api: {
-		bodyParser: false,
-	},
-};
-
-export default async function NewDrink(req: Request, res: Response) {
-	try {
-		upload.single('image')(req, res, (err) => {
-			if (err || !req.file) {
-				console.error(err);
-				return res.status(500).json('Файл не найден');
-			}
-			const { path } = req.file;
-			const pathWithExt = `${path}.jpg`;
-			console.log(pathWithExt);
-			return res
-				.status(200)
-				.json(pathWithExt.slice(6, pathWithExt.length).replaceAll('\\', '/'));
-		});
-	} catch (error) {
-		return res.status(500).json('Произошла ошибка при запросе');
-	}
+export async function PUT(request: Request) {
+  const { url } = await put('avatars/user-12345.png', request.body, { access: 'public' });
+ 
+  return Response.json({ url });
 }
