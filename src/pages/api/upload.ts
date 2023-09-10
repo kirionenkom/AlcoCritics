42f6a166273f1cd012ba5fa5b1faaeb84037eb63
Kirionenko <misha.kirionenko03@gmail.com>
2023-09-10 13:50:18 +0500

    Version 1.0

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -3,7 +3,7 @@ import { put } from '@vercel/blob';
 
 export const runtime = 'edge';
 
-export async function PUT(req: Request, res: Response) {
+export default async function PUT(req: Request, res: Response) {
 	const { url } = await put('avatars/user-12345.png', req.body, {
 		access: 'public',
 	});
